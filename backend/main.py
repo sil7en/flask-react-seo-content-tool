11762413f486114139e79d65d6f0fb42eaf54f1c
sil7en <silten@poptaim.com>
2022-11-15 21:03:43 -0300

    react flask base connection

--- backend/main.py
+++ backend/main.py
@@ -3,13 +3,10 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
-api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-
-api.add_resource(HelloWorld, '/')
+@app.route("/keywords")
+def keywords():
+    return {"keywords": ["keyword 1","keyword 2","keyword 3"]}
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const api = async () => {
+      const getData = await fetch('/keywords', {method: "GET"});
+      const jsonData = await getData.json();
+      setData(jsonData.keywords);
+    }
+
+    api();
+  }, []);
+
+
   return (
     <div>
       <p className='font-bold text-3xl'>
         Hola soy un párrafo estilizado con Tailwind
       </p>
+      {(typeof data === 'undefined') ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          {data.map((e) => {
+            return(
+              <span>{e}</span>
+            )
+          })}
+        </div>
+      )}
     </div>
   );
 }
